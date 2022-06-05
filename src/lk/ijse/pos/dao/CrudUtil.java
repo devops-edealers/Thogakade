4ad75bf36b9925c2828a572282b6b076115c953d
edealers <edealers.info@gmail.com>
2022-06-04 23:01:52 -0700

    5

---
+++ src/lk/ijse/pos/dao/CrudUtil.java
@@ -0,0 +1,20 @@
+package lk.ijse.pos.dao;
+
+import lk.ijse.pos.db.DatabaseConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CrudUtil {
+    public static <T> T execute(String sql, Object... params) throws SQLException, ClassNotFoundException {
+        PreparedStatement statement = DatabaseConnection.getInstance().getConnection()
+                .prepareStatement(sql);
+        for (int i = 0; i < params.length; i++) {
+            statement.setObject((i+1), params[i]);
+        }
+        if (sql.startsWith("SELECT")){
+            return (T) statement.executeQuery();
+        }
+        return (T)(Boolean)(statement.executeUpdate()>0);
+    }
+}
--- src/lk/ijse/pos/dao/DatabaseAccessCode.java
+++ src/lk/ijse/pos/dao/DatabaseAccessCode.java
@@ -1,5 +1,6 @@
 package lk.ijse.pos.dao;
 
+import lk.ijse.pos.db.DatabaseConnection;
 import lk.ijse.pos.dto.SystemUserDTO;
 
 import java.sql.*;
@@ -7,32 +8,16 @@ import java.sql.*;
 public class DatabaseAccessCode {
     public boolean createSystemUser(SystemUserDTO dto)
             throws ClassNotFoundException, SQLException {
-
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection connection=
-                DriverManager.getConnection(
-                        "jdbc:mysql://127.0.0.1:3306/Thogakade",
-                        "root",
-                        "1234");
+        Connection connection=DatabaseConnection.getInstance().getConnection();;
         String sql ="INSERT INTO system_user VALUES (?,?,?)";
         PreparedStatement statement = connection.prepareStatement(sql);
         statement.setString(1,dto.getName());
         statement.setString(2,dto.getEmail());
         statement.setString(3,dto.getPassword());
         return statement.executeUpdate()>0;
-       /* int saved = statement.executeUpdate();
-        if (saved>0){
-            return true;
-        }
-        return false;*/
     }
     public boolean login(String email, String password) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection connection=
-                DriverManager.getConnection(
-                        "jdbc:mysql://127.0.0.1:3306/Thogakade",
-                        "root",
-                        "1234");
+        Connection connection= DatabaseConnection.getInstance().getConnection();
         String sql ="SELECT * FROM system_user WHERE email =? AND password=?";
         PreparedStatement statement = connection.prepareStatement(sql);
         statement.setString(1,email);
