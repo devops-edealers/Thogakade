6c776b658ef3e003585d9490950b3055ccae3f1d
edealers <edealers.info@gmail.com>
2022-06-04 23:04:41 -0700

    6

--- src/lk/ijse/pos/dao/DatabaseAccessCode.java
+++ src/lk/ijse/pos/dao/DatabaseAccessCode.java
@@ -8,21 +8,14 @@ import java.sql.*;
 public class DatabaseAccessCode {
     public boolean createSystemUser(SystemUserDTO dto)
             throws ClassNotFoundException, SQLException {
-        Connection connection=DatabaseConnection.getInstance().getConnection();;
-        String sql ="INSERT INTO system_user VALUES (?,?,?)";
-        PreparedStatement statement = connection.prepareStatement(sql);
-        statement.setString(1,dto.getName());
-        statement.setString(2,dto.getEmail());
-        statement.setString(3,dto.getPassword());
-        return statement.executeUpdate()>0;
+        return CrudUtil.execute("INSERT INTO system_user VALUES (?,?,?)"
+                , dto.getName(), dto.getEmail(), dto.getPassword());
     }
+
     public boolean login(String email, String password) throws ClassNotFoundException, SQLException {
-        Connection connection= DatabaseConnection.getInstance().getConnection();
-        String sql ="SELECT * FROM system_user WHERE email =? AND password=?";
-        PreparedStatement statement = connection.prepareStatement(sql);
-        statement.setString(1,email);
-        statement.setString(2,password);
-        ResultSet resultSet = statement.executeQuery();
+        ResultSet resultSet =
+                CrudUtil.execute("SELECT * FROM system_user WHERE email =? AND password=?",
+                        email, password);
         return resultSet.next();
     }
 }
