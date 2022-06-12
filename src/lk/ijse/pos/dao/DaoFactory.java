dd422d602c49c3e8a0df2243d2e6a34493b94da6
edealers <edealers.info@gmail.com>
2022-06-11 22:08:36 -0700

    26

---
+++ src/lk/ijse/pos/dao/DaoFactory.java
@@ -0,0 +1,30 @@
+package lk.ijse.pos.dao;
+
+import lk.ijse.pos.dao.custom.impl.CustomerDaoImpl;
+import lk.ijse.pos.dao.custom.impl.ItemDaoImpl;
+
+public class DaoFactory {
+    private static DaoFactory daoFactory;
+    private DaoFactory(){}
+
+    public enum DaoType{
+        CUSTOMER,ITEM,ORDER,ORDER_DETAIL
+    }
+    public static DaoFactory getInstance(){
+        return ((daoFactory==null)? new DaoFactory():daoFactory);
+    }
+    public <T> T getDao(DaoType type){
+       switch (type){
+           case CUSTOMER:
+               return (T) new CustomerDaoImpl();
+           case ITEM:
+               return (T) new ItemDaoImpl();
+           case ORDER:
+               return null;
+           case ORDER_DETAIL:
+               return null;
+           default:
+               return null;
+       }
+    }
+}
